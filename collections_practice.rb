50e7e2d59e98f72cacd5a4be1a7fb45707716098
Christopher Luna <chris@illusionlayervfx.com>
2019-03-28 02:11:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  new_array = array[0], array[2], array[1]
+end
+
+def reverse_array(int)
+  int.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+
+  array.each do |name|
+    new_var = name.chars[2]
+   binding.pry
+    new_array << new_var.sub(2, "$")
+
+  end
+  new_array
+end
