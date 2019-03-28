8d012b9c26f5e0fdd65437b8c1cc219e550f8ea7
Christopher Luna <chris@illusionlayervfx.com>
2019-03-28 02:54:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,9 +24,9 @@ def kesha_maker(array)
   new_array = []
   string_replace = ["$"]
 
-  array.zip(string_replace).each do |pos, str|
+  array.zip(string_replace).each do |name, str|
     binding.pry
-  string[pos.to_i] = str
+  string[name.to_i] = str
 
   puts string
 end
